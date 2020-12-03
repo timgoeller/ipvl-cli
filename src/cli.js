b9dfe2e38f708eb6fb14397446211f17ae155914
Tim Göller <tim.goeller@posteo.de>
2020-12-03 15:39:21 +0100

    add basic cli interface structure

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,28 @@
-export function cli(args) {
-  console.log(args)
+const { Command } = require('commander')
+const FileLog = require('versioned-file-log')
+const IPFSStorageProviderHTTP = require('vfl-storage-provider-ipfs-http')
+const fs = require('fs-extra')
+const chalk = require('chalk')
+
+const log = console.log
+
+cli()
+
+function cli () {
+  const program = new Command()
+  program.version('0.0.1')
+  program
+    .command('publish <path> <version>')
+    .description('publish a file or directory')
+    .action((path, version) => {
+
+    })
+
+  program
+    .command('update <path> <version>')
+    .description('update a file or directory')
+    .action((path, version) => {
+
+    })
+  program.parse(process.argv)
 }
